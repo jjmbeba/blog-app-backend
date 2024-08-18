ba58e86f20556df8e24c0d75cec4733699a0e273
Johnson Mbeba <101251043+jjmbeba@users.noreply.github.com>
2024-08-18 21:48:41 +0300

    Add .env configurations

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db" 
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 # SQLALCHEMY_DATABASE_URL = "postgresql" + "://user:password@postgresserver/db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
